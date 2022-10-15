f01f8890231c536ce27fc8024f59df43c74c295b
tenadem <tenadem83@gmail.com>
2022-10-16 01:44:02 +0200

    added favorite table

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Comic } from '../model/comic';
+import { ComicService } from '../Services/comic.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
+  styleUrls: ['./favorites.component.css'],
 })
 export class FavoritesComponent implements OnInit {
+  allComics: Comic[] = [];
 
-  constructor() { }
+  constructor(private comicService: ComicService) {}
 
   ngOnInit(): void {
+    this.allComics = this.comicService.allComicFavorites;
   }
 
+  removeComic(id: number) {
+    const newAllComics = this.allComics.filter(
+      (comic: Comic) => comic.num !== id
+    );
+    this.comicService.allComicFavorites = newAllComics;
+    this.allComics = newAllComics;
+  }
 }
